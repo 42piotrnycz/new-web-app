2c3849aae7a56f509bf3484dbea88603177581f1
Piotr Nycz <147315956+42piotrnycz@users.noreply.github.com>
2025-03-17 22:39:39 +0100

    from-django-to-springboot

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # new-web-app
 
 Some of the links I'm using for testing.
+<b>
 Backend: http://localhost:8000/hello/
 Frontend: http://localhost:3000/
 PgAdmin: http://localhost:5050/ (login: admin@example.com, password: admin)
---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,24 @@
+package com.example.demo.service;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void registerUser(String username, String password) {
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(passwordEncoder.encode(password));
+        userRepository.save(user);
+    }
+}
